43ed1e7a93f8364489e1ce22ce416a39ff5d9fe4
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2025-12-27 16:57:59 -0500

    Time: 3 ms (66.33%), Space: 19 MB (95.91%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -3,23 +3,40 @@ class Solution:
         stack=[]
         ans=0
         for c in tokens:
-            if c in "+-*/":
-                a=stack.pop()
-                b=stack.pop()
-                stack.append(self.expression(c,b,a))
+            if c=="+":
+                stack.append(stack.pop()+stack.pop())
+            elif c=="-":
+                a,b=stack.pop(),stack.pop()
+                stack.append(b-a)
+            elif c=="*":
+                stack.append(stack.pop()*stack.pop())
+            elif c=="/":
+                a,b=stack.pop(),stack.pop()
+                stack.append(int(b/a))
             else:
                 stack.append(int(c))
         return stack[-1]
 
-    def expression(self,c,b,a):
-        if c=="+":
-            return b+a
-        elif c=="-":
-            return b-a
-        elif c=="*":
-            return b*a
-        else:
-            return int(b/a)
+
+
+    #     for c in tokens:
+    #         if c in "+-*/":
+    #             a=stack.pop()
+    #             b=stack.pop()
+    #             stack.append(self.expression(c,b,a))
+    #         else:
+    #             stack.append(int(c))
+    #     return stack[-1]
+
+    # def expression(self,c,b,a):
+    #     if c=="+":
+    #         return b+a
+    #     elif c=="-":
+    #         return b-a
+    #     elif c=="*":
+    #         return b*a
+    #     else:
+    #         return int(b/a)
 
 
 
