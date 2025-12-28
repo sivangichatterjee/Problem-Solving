0c5b4666526402211049f44d5fc3c1e2ec93be26
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2025-12-28 16:04:44 -0500

    Time: 3 ms (4.02%), Space: 17.5 MB (94.82%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,21 +1,48 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for l in matrix:
-            if self.binary_search(l, target)==1:
+    #complexity : O(m log n)
+    #     for l in matrix:
+    #         if self.binary_search(l, target)==1:
+    #             return True
+    #     return False
+
+    # def binary_search(self, l, target):
+    #     left, right=0,len(l)-1
+    #     while(left<=right):
+    #         mid=(left+right)//2
+    #         if target==l[mid]:
+    #             return 1
+    #         elif target>l[mid]:
+    #             left=mid+1
+    #         else:
+    #             right=mid-1
+    #     return -1
+    #Complexity; O(log m*n)
+        m,n=len(matrix),len(matrix[0])
+        top, bottom=0, m-1
+        while(top<=bottom):
+            row=(top+bottom)//2
+            if target>matrix[row][-1]:
+                top=row+1
+            elif target<matrix[row][0]:
+                bottom=row-1
+            else:
+                break
+
+        if top>bottom:
+            return False
+        l,r=0,n-1
+        while(l<=r):
+            mid=(l+r)//2
+            if target>matrix[row][mid]:
+                l=mid+1
+            elif target<matrix[row][mid]:
+                r=mid-1
+            else:
                 return True
         return False
 
-    def binary_search(self, l, target):
-        left, right=0,len(l)-1
-        while(left<=right):
-            mid=(left+right)//2
-            if target==l[mid]:
-                return 1
-            elif target>l[mid]:
-                left=mid+1
-            else:
-                right=mid-1
-        return -1
+
 
 
             
