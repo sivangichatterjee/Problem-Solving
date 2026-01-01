f4dd2ab1e4fbc13fb2d5177e1461d1c43cc4dce1
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-01-01 10:16:45 -0500

    Time: 3 ms (47.44%), Space: 22 MB (93.64%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,28 +8,48 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow,fast=head,head.next
-        while(fast and fast.next):
-            slow=slow.next
-            fast=fast.next.next
+        #O(n), O(1):time,space
+        # slow,fast=head,head.next
+        # while(fast and fast.next):
+        #     slow=slow.next
+        #     fast=fast.next.next
 
-        second=slow.next
-        slow.next=None
-        prev=None
-        #Reversing array
-        while second:
-            temp=second.next
-            second.next=prev
-            prev=second
-            second=temp
+        # second=slow.next #beginning of second list
+        # slow.next=None
+        # prev=None
+        # #Reversing second list
+        # while second:
+        #     temp=second.next
+        #     second.next=prev
+        #     prev=second
+        #     second=temp
 
-        first, second=head, prev
-        while second:
-            tmp1, tmp2=first.next, second.next
-            first.next=second
-            second.next=tmp1
-            first=tmp1
-            second=tmp2
+        # first, second=head, prev
+        # while second:
+        #     tmp1, tmp2=first.next, second.next
+        #     first.next=second
+        #     second.next=tmp1
+        #     first=tmp1
+        #     second=tmp2
+
+        curr=head
+        arr=[]
+        while curr:
+            arr.append(curr)
+            curr=curr.next
+        
+        i, j=0, len(arr)-1
+        while i<j:
+            arr[i].next=arr[j]
+            i+=1
+            if i>=j:
+                break
+            arr[j].next=arr[i]
+            j-=1
+            
+        arr[i].next=None
+
+
 
 
 
