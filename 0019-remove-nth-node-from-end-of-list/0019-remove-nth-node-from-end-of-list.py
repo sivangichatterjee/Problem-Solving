8696a7b8bb0f4da7fd45411b1a2eb6f432c9d63d
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-01-01 12:08:24 -0500

    Time: 0 ms (100%), Space: 17.4 MB (82.15%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,36 +5,49 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        curr=head
-        prev=None
-        while curr:
-            temp=curr.next
-            curr.next=prev
-            prev=curr
-            curr=temp
+        # curr=head
+        # prev=None
+        # while curr:
+        #     temp=curr.next
+        #     curr.next=prev
+        #     prev=curr
+        #     curr=temp
         
-        new_curr=prev
-        before=None
-        p=0
-        while new_curr:
-            p+=1           
-            if p==n:
-                if n==1:
-                    prev=prev.next
-                    break       
-                before.next=before.next.next
-            before=new_curr
-            new_curr=new_curr.next
+        # new_curr=prev
+        # before=None
+        # p=0
+        # while new_curr:
+        #     p+=1           
+        #     if p==n:
+        #         if n==1:
+        #             prev=prev.next
+        #             break       
+        #         before.next=before.next.next
+        #     before=new_curr
+        #     new_curr=new_curr.next
 
-        final_head=prev
-        bef=None
-        while prev:
-            t=prev.next
-            prev.next=bef
-            bef=prev
-            prev=t
+        # final_head=prev
+        # bef=None
+        # while prev:
+        #     t=prev.next
+        #     prev.next=bef
+        #     bef=prev
+        #     prev=t
 
-        return bef
+        # return bef
+        dummy=ListNode(0,head)
+        k,l,r=0,dummy,head
+        while n>0 and r:
+            r=r.next
+            n-=1
+
+        while r:
+            l=l.next
+            r=r.next
+
+        l.next=l.next.next
+        return dummy.next
+
 
 
         
