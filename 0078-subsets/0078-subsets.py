1cbcc0f13e4145b19227f7064281920fc04adc0e
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-01-09 10:21:11 -0500

    Time: 0 ms (100%), Space: 19.3 MB (16.03%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,29 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         subset, currset=[],[]
-        self.helper(0,nums, currset, subset)
-        return subset
+    #     self.helper(0,nums, currset, subset)
+    #     return subset
 
-    def helper(self, i, nums, currset, subset):
-        if i==len(nums):
-            subset.append(currset.copy())
-            return
+    # def helper(self, i, nums, currset, subset):
+    #     if i==len(nums):
+    #         subset.append(currset.copy())
+    #         return
         
-        currset.append(nums[i])
-        self.helper(i+1, nums, currset, subset)
-        currset.pop()
+    #     currset.append(nums[i])
+    #     self.helper(i+1, nums, currset, subset)
+    #     currset.pop()
 
-        self.helper(i+1, nums, currset, subset)
+    #     self.helper(i+1, nums, currset, subset)
+        def dfs(i):
+            if i==len(nums):
+                subset.append(currset.copy())
+                return
+            
+            currset.append(nums[i])
+            dfs(i+1)
+            currset.pop()
+            dfs(i+1)
+        dfs(0)
+        return subset
 
         
