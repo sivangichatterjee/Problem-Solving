adaada9674d15d11b08f009784919fbcf6319502
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-01-20 20:32:02 -0500

    Time: 7 ms (56.61%), Space: 22.5 MB (28.58%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -16,20 +16,41 @@ class Solution:
             dfs(r,c-1)
 
 
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if board[r][c]=="O" and (r in [0, ROWS-1] or c in [0,COLS-1]):
+        #            dfs(r,c)
+
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if board[r][c]=="O":
+        #             board[r][c]="X"
+
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if board[r][c]=="T":
+        #             board[r][c]="O"
+
+
         for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c]=="O" and (r in [0, ROWS-1] or c in [0,COLS-1]):
-                   dfs(r,c)
+            if board[r][0]=="O":
+                dfs(r,0)
+            if board[r][COLS-1]=="O":
+                dfs(r,COLS-1)
+
+        for c in range(COLS):
+            if board[0][c]=="O":
+                dfs(0,c)
+            if board[ROWS-1][c]=="O":
+                dfs(ROWS-1,c)
 
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c]=="O":
                     board[r][c]="X"
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c]=="T":
+                elif board[r][c]=="T":
                     board[r][c]="O"
+
               
 
         
