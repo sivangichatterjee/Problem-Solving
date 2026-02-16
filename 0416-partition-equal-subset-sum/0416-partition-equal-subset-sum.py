805b478fc0dd38fe1b2a0e4d1f13f08984e61fc3
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-02-15 19:20:12 -0500

    Time: 1576 ms (28.29%), Space: 35.1 MB (31.91%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -16,7 +16,7 @@ class Solution:
 
         for i in range(1, n+1):
             for j in range(1,target+1):
-                if nums[i-1]<=target:
+                if nums[i-1]<=j:
                     dp[i][j]=dp[i-1][j] or dp[i-1][j-nums[i-1]]
                 else:
                     dp[i][j]=dp[i-1][j]
