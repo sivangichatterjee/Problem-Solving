d67fb5818f4f82d02255d415eb56db40b9dd7b13
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-03-23 18:46:32 -0400

    Time: 32 ms (68.75%), Space: 31.3 MB (97.4%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,10 +1,31 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        #Kadane's algorithm
         maxSum=nums[0]
         curSum=0
+        # for n in nums:
+        #     curSum=max(curSum, 0)
+        #     curSum+=n
+        #     maxSum=max(curSum, maxSum)
+        # return maxSum
+
         for n in nums:
-            curSum=max(curSum, 0)
-            curSum+=n
+            curSum=max(curSum+n,n)
             maxSum=max(curSum, maxSum)
         return maxSum
+
+        #Sliding window variation
+        # l,r=0,0
+        # for r in range(len(nums)):
+        #     if curSum<0:
+        #         l=r
+        #         curSum=0
+
+        #     curSum+=nums[r]
+        #     if curSum>maxSum:
+        #         maxSum=curSum
+        #         maxL=l
+        #         maxR=r
+
+                 
         
