2a4c578fededd8cf63a2e7e7327cf194ddbf9b38
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-04-01 01:30:57 -0400

    Time: 7 ms (90.5%), Space: 19.6 MB (36.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,11 +3,11 @@ class Solution:
         count1=Counter(s)
         count2=Counter(t)
 
-        for i in range(len(s)):
-            count1[s[i]]+=1
+        # for i in range(len(s)):
+        #     count1[s[i]]+=1
 
-        for j in range(len(t)):
-            count2[t[j]]+=1
+        # for j in range(len(t)):
+        #     count2[t[j]]+=1
 
         return count1==count2 
         
