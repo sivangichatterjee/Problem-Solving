59678b484af4a3c0f53c44aca8adfeebfc6848f8
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-04-01 10:14:56 -0400

    Time: 3 ms (37.64%), Space: 19.3 MB (53.64%) - LeetHub

--- 0830-positions-of-large-groups/0830-positions-of-large-groups.py
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.py
@@ -1,15 +1,28 @@
 class Solution:
     def largeGroupPositions(self, s: str) -> List[List[int]]:
-        n=len(s)
-        start=0
+        # n=len(s)
+        # start=0
+        # large=[]
+        # for i in range(1,n+1):
+        #     if i==n or s[i]!=s[i-1]:
+        #         if i-start>=3:
+        #             large.append([start,i-1])
+        #         start=i
+
+        # return large
+
         large=[]
-        for i in range(1,n+1):
-            if i==n or s[i]!=s[i-1]:
-                if i-start>=3:
-                    large.append([start,i-1])
-                start=i
+        start=end=0
+        while start<len(s):
+            end=start
+            while end+1<len(s) and s[start]==s[end+1]:
+                end+=1
+            if end-start+1>=3:
+                large.append([start, end])
+            start=end+1
 
         return large
+
 
        
             
