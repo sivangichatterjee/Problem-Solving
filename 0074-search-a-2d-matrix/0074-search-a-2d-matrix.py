ae2edea4f7a16c1381567ef10e90d9ab0e3c1a37
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-04-04 14:29:48 -0400

    Time: 0 ms (100%), Space: 19.6 MB (26.42%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,38 +1,18 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-    #complexity : O(m log n)
-    #     for l in matrix:
-    #         if self.binary_search(l, target)==1:
-    #             return True
-    #     return False
-
-    # def binary_search(self, l, target):
-    #     left, right=0,len(l)-1
-    #     while(left<=right):
-    #         mid=(left+right)//2
-    #         if target==l[mid]:
-    #             return 1
-    #         elif target>l[mid]:
-    #             left=mid+1
-    #         else:
-    #             right=mid-1
-    #     return -1
-    #Complexity; O(log m*n)
         m,n=len(matrix),len(matrix[0])
-        top, bottom=0, m-1
-        while(top<=bottom):
+        top,bottom=0, m-1
+        while top<=bottom:
             row=(top+bottom)//2
-            if target>matrix[row][-1]:
-                top=row+1
-            elif target<matrix[row][0]:
+            if target<matrix[row][0]:
                 bottom=row-1
+            elif target>matrix[row][-1]:
+                top=row+1
             else:
                 break
 
-        if top>bottom:
-            return False
         l,r=0,n-1
-        while(l<=r):
+        while l<=r:
             mid=(l+r)//2
             if target>matrix[row][mid]:
                 l=mid+1
@@ -40,10 +20,11 @@ class Solution:
                 r=mid-1
             else:
                 return True
+
+
+
         return False
 
+        
 
-
-
-            
         
