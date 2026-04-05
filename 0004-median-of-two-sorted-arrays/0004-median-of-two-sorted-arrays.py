e871d56535eb37a29d2c9d314fa7a18a08e69c14
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-04-05 11:53:25 -0400

    Time: 0 ms (100%), Space: 19.5 MB (58.68%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,37 +1,31 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # nums1.extend(nums2)
-        # nums1.sort()
-        # n=len(nums1)
-        # if n%2==0:
-        #     return ((nums1[n//2]+nums1[(n//2)-1])/2)
-        # else:
-        #     return nums1[n//2]
-
+        A,B=nums1,nums2
+        if len(nums2)<len(nums1):
+            A,B=B,A
         total=len(nums1)+len(nums2)
         half=total//2
-        if len(nums1)<len(nums2):
-            A,B=nums1,nums2
-        else:
-            B,A=nums1, nums2
 
         l,r=0,len(A)-1
+
         while True:
-            m=(l+r)//2 # middle index for A
-            n=half-m-2 # B ->since indices start from 0 
-            Aleft=A[m] if m>=0 else float("-infinity")
-            Aright=A[m+1] if m+1<len(A) else float("infinity")
-            Bleft=B[n] if n>=0 else float("-infinity")
-            Bright=B[n+1] if n+1<len(B) else float("infinity")
+            i=(l+r)//2
+            j=half-i-2
 
-            if Aleft<=Bright and Aright>=Bleft:
+            Aleft=A[i] if i>=0 else float("-infinity")
+            Aright=A[i+1] if (i+1)<len(A) else float("infinity")
+            Bleft=B[j] if j>=0 else float("-infinity")
+            Bright=B[j+1] if (j+1)<len(B) else float("infinity")
+
+            if Aleft<=Bright and Bleft<=Aright:
                 if total%2:
-                    return min(Aright, Bright)
-                return (max(Aleft, Bleft)+min(Aright,Bright))/2
+                    return min(Aright,Bright)
+                else:
+                    return (max(Aleft,Bleft)+min(Aright,Bright))/2
             elif Aleft>Bright:
-                r=m-1
+                r=i-1
             else:
-                l=m+1
-
+                l=i+1
+            
 
         
