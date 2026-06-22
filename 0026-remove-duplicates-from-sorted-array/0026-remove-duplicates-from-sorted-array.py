8ca6a69a6102288261934e5069d8e9602fb0c63b
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-06-22 14:27:00 -0400

    Time: 1 ms (56.64%), Space: 20.6 MB (42.36%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -10,12 +10,20 @@ class Solution:
 
         # return len(nums)
 
-        l=1
-        for r in range(1,len(nums)):
-            if nums[r]!=nums[r-1]:
-                nums[l]=nums[r]
-                l+=1
+        # l=1
+        # for r in range(1,len(nums)):
+        #     if nums[r]!=nums[r-1]:
+        #         nums[l]=nums[r]
+        #         l+=1
 
+        # return l
+
+        l,r=1,0
+        for r in range(len(nums)-1):
+            if nums[r]!=nums[r+1]:
+                nums[l]=nums[r+1]
+                l+=1
         return l
+
 
         
