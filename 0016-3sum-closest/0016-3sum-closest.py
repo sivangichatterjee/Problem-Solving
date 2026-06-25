188be2ed8b36ef844ea0afae27ec80bf71ec8d21
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-06-25 09:31:50 -0400

    Time: 369 ms (77.59%), Space: 19.3 MB (80.96%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -3,16 +3,10 @@ class Solution:
         nums.sort()
         threesum,ans=0,0
         minsum=float("inf")
-
-        for i in range(len(nums)):
-
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-            
+        for i in range(len(nums)):   
             l,r=i+1,len(nums)-1
             while l<r:
                 threesum=nums[i]+nums[l]+nums[r]
-                #minsum=min(minsum, abs(threesum-target))
                 if target>threesum:
                     l+=1
                 elif target<threesum:
@@ -23,8 +17,6 @@ class Solution:
                 if abs(threesum-target)<minsum:
                     minsum=abs(threesum-target)
                     ans=threesum
-                # l+=1
-                # r-=1
 
         return ans
         
