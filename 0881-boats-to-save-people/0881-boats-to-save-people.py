a3d28fdffbbab5c7102b66396e1e9c7b1f81212e
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-06-29 12:44:18 -0400

    Time: 50 ms (68.05%), Space: 25.2 MB (34.6%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -4,7 +4,7 @@ class Solution:
         l,r=0,len(people)-1
         ct=0
         while l<=r:
-            if l!=r and people[l]+people[r]<=limit:
+            if people[l]+people[r]<=limit:
                 l+=1
                 r-=1
                 ct+=1
@@ -12,17 +12,6 @@ class Solution:
                 r-=1
                 ct+=1
         return ct
-        # for i, r in enumerate(people):
-        #     sum+=r
-        #     if sum==limit:
-        #         sum=0
-        #         ct+=1
-        #     elif sum>limit:
-        #         ct+=1
-        #         sum=r           
-
-
-        # return ct
-
+       
 
         
