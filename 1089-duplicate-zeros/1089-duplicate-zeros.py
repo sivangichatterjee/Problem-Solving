a8239ff5d917b061507cfb901cca99f799aeda93
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-07-01 14:06:47 -0400

    Time: 3 ms (63.11%), Space: 19.4 MB (32.66%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -4,20 +4,32 @@ class Solution:
         Do not return anything, modify arr in-place instead.
         """
 
+        # zeroes=arr.count(0)
+        # n=len(arr)
+        # read=n-1
+        # write=n+zeroes-1
+        # while read>=0:
+        #     if write<n:
+        #         arr[write]=arr[read]
+        #     if arr[read]==0:
+        #         write-=1
+        #         if write<n:
+        #             arr[write]=0
+
+        #     write-=1
+        #     read-=1
         zeroes=arr.count(0)
         n=len(arr)
-        read=n-1
-        write=n+zeroes-1
-        while read>=0:
-            if write<n:
-                arr[write]=arr[read]
-            if arr[read]==0:
-                write-=1
-                if write<n:
-                    arr[write]=0
+        result=[]
+        for i in range(len(arr)):
+            if len(result)<n:
+                result.append(arr[i])
+            if arr[i]==0 and len(result)<n:
+                result.append(0)
+            
+        for i in range(len(result)):
+            arr[i]=result[i]
 
-            write-=1
-            read-=1
 
 
         
