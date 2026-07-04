62a6638d21e65552803fc542b5800417ab85ce32
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-07-04 19:05:46 -0400

    Time: 3 ms (41.77%), Space: 19.3 MB (34.46%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -6,17 +6,20 @@ class Solution:
                 s[start],s[end]=s[end],s[start]
                 start+=1
                 end-=1
+
+        for l in range(0,len(s),2*k):
+            reverse(l,min(l+k-1,len(s)-1))
         
-        if len(s)<k:
-            reverse(0,len(s)-1)
-        elif len(s)>k and len(s)<=2*k:
-            reverse(0,k-1)
-        else:
-            l,r=0,k-1            
-            while l<len(s):
-                reverse(l,r)
-                l+=2*k
-                r=min(l + k - 1, len(s) - 1)
+        # if len(s)<k:
+        #     reverse(0,len(s)-1)
+        # elif len(s)>k and len(s)<=2*k:
+        #     reverse(0,k-1)
+        # else:
+        #     l,r=0,k-1            
+        #     while l<len(s):
+        #         reverse(l,r)
+        #         l+=2*k
+        #         r=min(l + k - 1, len(s) - 1)
 
         return "".join(s)
 
