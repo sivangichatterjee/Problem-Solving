11b41bb777774566284ea662700c7df0c0c212e3
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-07-09 22:30:15 -0400

    Time: 23 ms (48.28%), Space: 19.2 MB (85.87%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,45 +1,39 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        #complexity: O(m * n log n), m = len(s2), n = len(s1)
-        # new_s1=''.join(sorted(s1))
-        # n=len(s1)
-        # for i in range(len(s2)-n+1):
-        #     if ''.join(sorted(s2[i:i+n]))==new_s1:
-        #         return True
-        # return False
-        if len(s1) > len(s2):
+        if len(s1)>len(s2):
             return False
-        s1Count, s2Count= [0]*26, [0]*26
-        for i in range(len(s1)):
-            s1Count[(ord(s1[i])- ord('a'))]+=1
-            s2Count[(ord(s2[i])- ord('a'))]+=1
 
+        s1count,s2count=defaultdict(int),defaultdict(int)
         matches=0
-        for i in range(26):
-            if s1Count[i]==s2Count[i]:
-                matches+=1
-        
+
+        for i in range(len(s1)):
+            s1count[s1[i]]+=1
+            s2count[s2[i]]+=1
+
         l=0
-        for r in range(len(s1), len(s2)):
-            if matches==26:
+        keys=set(s1count)|set(s2count)
+        matches=sum(1 for c in s1count if s1count[c]==s2count[c])
+        for r in range(len(s1),len(s2)):
+            if matches==len(s1count):
                 return True
-            index=ord(s2[r]) - ord('a')
-            s2Count[index]+=1
-            if s2Count[index]==s1Count[index]:
-                matches+=1
-            elif s2Count[index] == s1Count[index] + 1:
-                matches-=1
 
-            index=ord(s2[l]) - ord('a')
-            s2Count[index]-=1
-            if s2Count[index]==s1Count[index]:
-                matches+=1
-            elif s2Count[index] == s1Count[index] - 1:
-                matches-=1
+            c=s2[r]
+            s2count[c]+=1
+            if c in s1count:
+                if s1count[c]==s2count[c]:
+                    matches+=1
+                elif s1count[c]+1==s2count[c]:
+                    matches-=1
+
+            c=s2[l]
+            s2count[c]-=1
+            if c in s1count:
+                if s1count[c]==s2count[c]:
+                    matches+=1
+                elif s1count[c]-1==s2count[c]:
+                    matches-=1
+
             l+=1
-        return matches==26
 
-
-
+        return matches==len(s1count)
         
-            
