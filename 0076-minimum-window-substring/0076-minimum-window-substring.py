16d4dc874a9d7f192ef35fe1686495483223d3ee
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-07-13 12:07:06 -0400

    Time: 58 ms (87.35%), Space: 19.6 MB (93.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,36 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        window, count={},{}
-        minlen=float("infinity")
-        res=""
-        if t=="": return ""
-
+        countT, window=defaultdict(int),defaultdict(int)
         for i in range(len(t)):
-            count[t[i]]=1+count.get(t[i],0)
+            countT[t[i]]+=1
 
-        need, have=len(count), 0
-
+        have, need=0,len(countT)
         l=0
+        res,resLen=[-1,-1],float("inf")
+
         for r in range(len(s)):
             c=s[r]
-            window[c]=1+window.get(c,0)
-
-            if c in count and window[c]==count[c]:
+            window[c]+=1
+            if c in countT and countT[c]==window[c]:
                 have+=1
-            
             while have==need:
-                if r-l+1 < minlen:
-                    res=s[l:r+1]
-                    minlen=r-l+1                   
-                    # l+=1
-                if s[l] in count and window[s[l]]==count[s[l]]:
-                    have-=1
+                if (r-l+1)<resLen:
+                    res=[l,r]
+                    resLen=r-l+1
                 window[s[l]]-=1
+                if s[l] in countT and window[s[l]]<countT[s[l]]:
+                    have-=1
                 l+=1
 
-        return res 
-        
-            
+        l,r=res
+        return s[l:r+1] if resLen !=float("inf") else ""
 
 
         
