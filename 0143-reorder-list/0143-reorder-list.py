2317336d1a7644b71970c3baef17142bdfd658f5
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-07-26 11:54:20 -0400

    Time: 0 ms (100%), Space: 27.7 MB (96.95%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,53 +8,18 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        #O(n), O(1):time,space
-        # slow,fast=head,head.next
-        # while(fast and fast.next):
-        #     slow=slow.next
-        #     fast=fast.next.next
-
-        # second=slow.next #beginning of second list
-        # slow.next=None
-        # prev=None
-        # #Reversing second list
-        # while second:
-        #     temp=second.next
-        #     second.next=prev
-        #     prev=second
-        #     second=temp
-
-        # first, second=head, prev
-        # while second:
-        #     tmp1, tmp2=first.next, second.next
-        #     first.next=second
-        #     second.next=tmp1
-        #     first=tmp1
-        #     second=tmp2
-
-        curr=head
         arr=[]
+        curr=head
         while curr:
             arr.append(curr)
             curr=curr.next
-        
-        i, j=0, len(arr)-1
+
+        i,j=0,len(arr)-1
         while i<j:
             arr[i].next=arr[j]
             i+=1
-            if i>=j:
-                break
             arr[j].next=arr[i]
             j-=1
-            
         arr[i].next=None
-
-
-
-
-
-
-
-        
-        
+            
         
