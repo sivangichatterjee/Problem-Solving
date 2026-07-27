a94ef22a2d835168750ebf5c2edb68fe4657ad16
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-07-26 22:40:51 -0400

    Time: 171 ms (11.31%), Space: 76.9 MB (60.64%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,42 +1,44 @@
 class Node:
     def __init__(self, key, val):
-        self.key, self.val=key,val
-        self.prev= self.next=None
+        self.key,self.val=key,val
+        self.prev=self.next=None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cap=capacity
+        self.capacity=capacity
         self.cache={}
         self.left, self.right=Node(0,0), Node(0,0)
         self.left.next, self.right.prev=self.right, self.left
 
-    def remove(self,node):
-        prev, nxt= node.prev, node.next
-        prev.next, nxt.prev=nxt, prev
+    def remove(self, node):
+        prev, next=node.prev, node.next
+        prev.next, next.prev=next, prev
 
+    
     def insert(self, node):
         prev, nxt=self.right.prev, self.right
         prev.next=nxt.prev=node
-        node.next,node.prev=nxt,prev
+        node.next,node.prev=self.right, prev
+        
 
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
+            return self.cache[key].val #self.cache[key] points to the Node .val points value
+        return -1   
         
-
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key]=Node(key, value)
-        self.insert(self.cache[key])
-        if len(self.cache)>self.cap:
+        self.insert(self.cache[key])  # insert in the linked list
+
+        if len(self.cache)>self.capacity:
             lru=self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-
         
 
 
