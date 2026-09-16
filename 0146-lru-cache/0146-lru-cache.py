4cd6b58ca34a75664fcf33e16c1baebf19923ae9
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-09-16 17:22:27 -0400

    Time: 31 ms (68.15%), Space: 29.4 MB (75.88%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,44 +1,48 @@
 class Node:
     def __init__(self, key, val):
-        self.key,self.val=key,val
+        self.val,self.key=val,key
         self.prev=self.next=None
+
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity=capacity
-        self.cache={}
+        self.cache={} #key->Node
+        self.cap=capacity
         self.left, self.right=Node(0,0), Node(0,0)
-        self.left.next, self.right.prev=self.right, self.left
+        self.left.next,self.right.prev=self.right,self.left
+        
 
     def remove(self, node):
-        prev, next=node.prev, node.next
-        prev.next, next.prev=next, prev
+        prev,next=node.prev, node.next
+        prev.next,next.prev=next,prev
 
-    
+
     def insert(self, node):
-        prev, nxt=self.right.prev, self.right
-        prev.next=nxt.prev=node
-        node.next,node.prev=self.right, prev
-        
+        prev, next=self.right.prev,self.right
+        prev.next=next.prev=node
+        node.prev,node.next=prev,next
+
 
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            return self.cache[key].val #self.cache[key] points to the Node .val points value
-        return -1   
+            return self.cache[key].val
+        return -1
         
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        self.cache[key]=Node(key, value)
-        self.insert(self.cache[key])  # insert in the linked list
-
-        if len(self.cache)>self.capacity:
+        self.cache[key]=Node(key,value)
+        self.insert(self.cache[key])
+        
+        if len(self.cache)>self.cap:
             lru=self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+        
         
 
 
