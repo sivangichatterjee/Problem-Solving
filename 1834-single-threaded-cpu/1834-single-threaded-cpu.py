c3e6145595bc146d495fd28bdb6bc832da38c93e
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-09-17 16:17:10 -0400

    Time: 617 ms (25.18%), Space: 63.4 MB (65.24%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -2,21 +2,21 @@ class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         for i,t in enumerate(tasks):
             t.append(i)
-        #enqueue, time taken, original index
+
         tasks.sort(key=lambda x:x[0])
-        minHeap,res=[],[]
-        time=tasks[0][0]
+        res,minHeap=[],[]
         i=0
+        time=tasks[0][0]
+
         while minHeap or i<len(tasks):
             while i<len(tasks) and time>=tasks[i][0]:
-                heapq.heappush(minHeap, [tasks[i][1], tasks[i][2]])
+                heapq.heappush(minHeap, [tasks[i][1],tasks[i][2]])
                 i+=1
 
             if not minHeap:
                 time=tasks[i][0]
-
             else:
-                proctime,index=heapq.heappop(minHeap)
+                proctime, index=heapq.heappop(minHeap)
                 time+=proctime
                 res.append(index)
 
