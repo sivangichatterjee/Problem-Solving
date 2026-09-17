f2f7bdd03b7dd8b25fa6515a8270c37b6471085a
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-09-17 10:08:13 -0400

    Time: 7 ms (52.29%), Space: 25 MB (6.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,22 +1,11 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #O(n log n)
-        # count=Counter(nums)
-
-        # count_sorted= sorted(count.items(), key=lambda x:x[1], reverse=True)
-        # ans=[item for item,count in count_sorted[:k]]
-
-        # return ans
-
-        #O(n)
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
         freq=[[] for _ in range(len(nums)+1)]
-        result=[]
-        count=defaultdict(int)
-        for n in nums:
-            count[n]+=1
-        
-        for n,c in count.items():
+        count=Counter(nums)
+        for n, c in count.items():
             freq[c].append(n)
+
+        result=[]
 
         for i in range(len(freq)-1,-1,-1):
             for n in freq[i]:
@@ -24,38 +13,6 @@ class Solution:
                 if len(result)==k:
                     return result
 
-
-       
         
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        #  count=Counter(nums)
-
-        # sorted_items = sorted(count.items(), key=lambda x: x[1], reverse=True)
-        # ans=[item for item, count in sorted_items[:k]]
-
-        # return ans
-                
+        
