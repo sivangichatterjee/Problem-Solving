6612785232a5417c1b597511e461fddae4c7c031
Sivangi Chatterjee <83332106+sivangichatterjee@users.noreply.github.com>
2026-09-17 19:47:24 -0400

    Time: 3 ms (8.22%), Space: 19.3 MB (94.79%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,31 +1,34 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         adj=defaultdict(list)
-
-        for i,eq in enumerate(equations):
+        for i, eq in enumerate(equations):
             a,b=eq
-            adj[a].append([b,values[i]])
-            adj[b].append([a,1/values[i]])
+            adj[a].append([b, values[i]])
+            adj[b].append([a, 1/values[i]])
 
         ans=[]
         def bfs(src,target):
             if src not in adj or target not in adj:
                 return -1
             q, visit=deque(),set()
-            q.append([src, 1])
+            q.append([src,1])
             visit.add(src)
+
             while q:
                 n,w=q.popleft()
-                if n==target:
+                if target==n:
                     return w
-                for nei,weight in adj[n]:
+
+                for nei, weight in adj[n]:
                     if nei not in visit:
-                        q.append([nei,weight*w])
+                        q.append([nei, weight*w])
                         visit.add(nei)
+
             return -1
 
         for q in queries:
             ans.append(bfs(q[0],q[1]))
 
         return ans
+
         
